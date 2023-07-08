074190f470794776777b06d12c245bd5ba49b997
hasezoey <hasezoey@gmail.com>
2023-07-08 18:46:53 +0200

    refactor(lib::code): remove unused variable

--- crates/lib/src/code.rs
+++ crates/lib/src/code.rs
@@ -228,7 +228,6 @@ impl<'a> Struct<'a> {
     fn render(&mut self) {
         let ty = self.ty;
         let table = &self.table;
-        let _opts = self.config.table(table.name.to_string().as_str());
 
         let primary_keys: Vec<String> = table.primary_key_column_names();
 
